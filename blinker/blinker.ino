307690052e71b2b3b634ff330f8b3f9b857f89e6
Matthew Kenny <matthewkenny@gmail.com>
2014-01-11 17:32:57 -0800

    blinker

---
+++ blinker/blinker.ino
@@ -0,0 +1,15 @@
+// blinker.ino
+
+const int LED = 13;
+
+void setup() {
+    pinMode(LED, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(500);
+}
+
